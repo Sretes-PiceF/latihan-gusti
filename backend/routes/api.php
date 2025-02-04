f0093043025d6033b8a49e98fe24330501aa406f
SingaBAung <7afajarihsan@gmail.com>
2025-02-04 22:26:05 +0700

    menyelesaikan API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Resources\User;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::get(uri: '/user_biasa', action: function(): AnonymousResourceCollection{
+    return UserResource::collection(resource: User::all());
 });
